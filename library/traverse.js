51f0b66a5d19de87136860099f4b8f01c266d5d6
Mahesh Ruparel <maheshruparel@gmail.com>
2020-09-16 23:09:23 +0530

    Create traverse.js

---
+++ library/traverse.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const traverse = function (filePath, handler) {
+  fs.readdirSync(filePath).forEach(function (file) {
+    let file = path.join(filePath, file);
+    if (fs.statSync(file).isDirectory()) {
+      exports.travel(file, handler);
+    } else {
+      handler(file);
+    }
+  });
+};
+
+export default traverse;
