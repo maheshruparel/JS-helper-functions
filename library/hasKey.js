ecbf2e7f1b66697be6cf99048c44f00a0ba9b1e0
Mahesh Ruparel <maheshruparel@gmail.com>
2020-10-25 23:49:52 +0530

    Update hasKey.js

--- library/hasKey.js
+++ library/hasKey.js
@@ -1,9 +1,22 @@
+// const hasKey = (obj, keys) => {
+//   if (typeof obj === 'object' || obj.hasOwnProperty(key)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+
 const hasKey = (obj, keys) => {
-  if (typeof obj === 'object' || obj.hasOwnProperty(key)) {
-    return true;
-  } else {
-    return false;
-  }
+  return (
+    keys.length > 0 &&
+    keys.every(key => {
+      if (typeof obj !== 'object' || !obj.hasOwnProperty(key)) return false;
+      obj = obj[key];
+      return true;
+    })
+  );
 };
+
 
 export default hasKey
